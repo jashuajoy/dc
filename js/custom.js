22122bf94e69f9f1b4df6b3ff0eeb8bb5d04ba4c
srinumadhavv <43718077+srinumadhavv@users.noreply.github.com>
2020-02-29 11:46:36 +0530

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,18 @@
+ var $form = $('form#test-form'),
+    url = 'https://script.google.com/macros/s/AKfycbx12jFRwx3IuszEUjsAJ6MaIcewzEIL5coJCV9modAfL3Zwpw/exec'
+
+$('#submit-form').on('click', function(e) {
+  e.preventDefault();
+  var jqxhr = $.ajax({
+    url: url,
+    method: "GET",
+    dataType: "json",
+    data: $form.serializeObject()
+  }).success(
+    // do something
+    alert('sucess'),
+  );
+});
 
 (function($) {
     // 'use strict';
